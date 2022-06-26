26edcdfbfa05af62e2450d04d94a0447ab0c577d
Dheovani <dheovani_xavier@outlook.com>
2022-06-26 16:57:49 -0300

    Adicionando comentários

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -39,6 +39,7 @@ class HeapSort {
             int lchild = (root * 2) + 1;
             int rchild = (root * 2) + 2;
 
+            // Select the biggest value in the given positions
             max = ((lchild < _size) && (arr[lchild] > arr[max])) ? lchild : max;
             max = ((rchild < _size) && (arr[rchild] > arr[max])) ? rchild : max;
 
@@ -97,10 +98,12 @@ class HeapSort {
         void sort(int arr[], int _size) {
 
             int i;
+            // Build heap in the non-leaf elements
             for(i = (_size / 2) - 1; i >= 0; i --) {
                 heapify(arr, i, _size);
             }
 
+            // Sort elements starting from root
             for(i = _size - 1; i > 0; i --) {
                 swap(arr, 0, i);
                 heapify(arr, 0, i);
