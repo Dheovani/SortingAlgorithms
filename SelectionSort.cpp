d1b427139215465312bc209a81d3b0cfcccc498b
Dheovani <dheovani_xavier@outlook.com>
2022-06-26 20:21:03 -0300

    Criando classe SelectionSort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class InsertionSort {
+class SelectionSort {
 
     private:
 
@@ -67,25 +67,17 @@ class InsertionSort {
          */
         void sort(int arr[], int _size) {
 
-            if(isEmpty(arr, _size)) {
-                cout << "Array is empty" << endl;
-                return;
-            }
-            
-            int cont, aux, i;
-            for(i = 0; i < _size; i ++) {
-                cont = 1;
-                aux = 0;
+            int small, i, j;
+            for(i = 0; i < _size - 1; i ++) {
+                small = i;
 
-                // Only swap when i != 0 and the previous value is bigger
-                if(i != 0) {
-                    while(i >= cont && arr[i - cont] > arr[i - aux]) {
-                        printArray(arr, _size);
-                        swap(arr, (i - aux), (i - cont));
-                        cont ++;
-                        aux ++;
+                for(j = i + 1; j < _size; j ++) {
+                    if(arr[small] > arr[j]) {
+                        small = j;
                     }
                 }
+
+                swap(arr, i, small);
             }
 
         }
@@ -95,15 +87,15 @@ class InsertionSort {
 int main() {
 
     int i, _size, arr[10];
-    InsertionSort* insertionSort = new InsertionSort();
+    SelectionSort* selectionSort = new SelectionSort();
     _size = sizeof(arr) / sizeof(arr[0]);
 
     for(i = 0; i < _size; i ++) {
         arr[i] = (rand() % 100) + 1;
     }
 
-    insertionSort->sort(arr, _size);
-    insertionSort->printArray(arr, _size);
+    selectionSort->sort(arr, _size);
+    selectionSort->printArray(arr, _size);
 
     return 0;
 
