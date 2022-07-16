5fe2235ab01abbb2a1e43691b318719b1497e5c2
Dheovani <ag.dhoxdc@gmail.com>
2022-07-16 16:38:46 -0300

    Iniciando criação das novas classes

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,4 +5,18 @@ using namespace std;
 
 class MergeSort : public Sort {
 
+    public:
+        /**
+         * @brief Sort the array
+         * 
+         * @param arr 
+         * @param _size 
+         */
+        void sortArray(int arr[], int _size) {
+            if(isEmpty(arr, _size)) {
+                cout << "Array is empty" << endl;
+                return;
+            }
+        }
+
 };
--- QuickSort.cpp
+++ QuickSort.cpp
@@ -5,4 +5,18 @@ using namespace std;
 
 class QuickSort : public Sort {
 
+    public:
+        /**
+         * @brief Sort the array
+         * 
+         * @param arr 
+         * @param _size 
+         */
+        void sortArray(int arr[], int _size) {
+            if(isEmpty(arr, _size)) {
+                cout << "Array is empty" << endl;
+                return;
+            }
+        }
+
 };
